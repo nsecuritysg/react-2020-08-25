6dbfd9a5b4070df674232da214fbc4c509a30dbc
nsecuritysg <40561053+nsecuritysg@users.noreply.github.com>
2020-09-07 20:16:00 +0300

    HT2

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './navigation.module.css';
 
 const Navigation = ({ restaurants, onRestaurantClick }) => (
@@ -14,5 +15,10 @@ const Navigation = ({ restaurants, onRestaurantClick }) => (
     ))}
   </div>
 );
+
+Navigation.propTypes = {
+  restaurants: PropTypes.array.isRequired,
+  onRestaurantClick: PropTypes.func.isRequired,
+};
 
 export default Navigation;
--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -27,4 +27,11 @@ describe('Product', () => {
     mount(<Product product={product} fetchData={fn} />);
     expect(fn).toBeCalledWith(product.id);
   });
+  it('should decrement amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper.find('[data-id="product-increment"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+  });
 });
